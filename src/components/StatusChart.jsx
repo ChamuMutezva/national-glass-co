09acb40de7d4275ebca4b6b0a1e9425806a464cb
Chamu Mutezva <ckmutezva@gmail.com>
2024-03-10 12:27:36 +0200

    chart improved using recharts

--- src/components/StatusChart.jsx
+++ src/components/StatusChart.jsx
@@ -1,47 +1,185 @@
-import { BarPlot } from "@mui/x-charts/BarChart";
-import { LinePlot } from "@mui/x-charts/LineChart";
-import { ResponsiveChartContainer } from "@mui/x-charts";
-import { Box } from "@mui/material";
-
-import { ChartsXAxis } from "@mui/x-charts/ChartsXAxis";
-import { ChartsYAxis } from "@mui/x-charts/ChartsYAxis";
 import {
-    BROKENGLASS,
-    DATATIME,
-    TARGETDATA,
-    TOUGHENEDGLASS,
-    AVERAGEDATA,
-} from "../data";
+    ComposedChart,
+    Line,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from "recharts";
+import { Box } from "@mui/material";
 import TougheningOptions from "./TougheningOptions";
 
-const series = [
+const data = [
     {
-        type: "bar",
-        stack: "A",
-        yAxisKey: "eco",
-        label: "Broken",
-        data: BROKENGLASS,
+        name: "00:00",
+        toughened: 624,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
     },
     {
-        type: "bar",
-        stack: "A",
-        yAxisKey: "eco",
-        label: "Toughened",
-        data: TOUGHENEDGLASS,
+        name: "01:00",
+        toughened: 583,
+        target: 1000,
+        average: 157.92,
+        broken: 118,
     },
     {
-        type: "line",
-        label: "target",
-        yAxisKey: "eco",
-        color: "red",
-        data: TARGETDATA,
+        name: "02:00",
+        toughened: 701,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
     },
     {
-        type: "line",
-        label: "average",
-        yAxisKey: "eco",
-        color: "black",
-        data: AVERAGEDATA,
+        name: "03:00",
+        toughened: 144,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "04:00",
+        toughened: 677,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "05:00",
+        toughened: 663,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "06:00",
+        toughened: 286,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "07:00",
+        toughened: 52,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "08:00",
+        toughened: 60,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "09:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "10:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "11:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "12:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "13:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "14:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "15:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "16:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "17:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "18:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "19:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "20:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "21:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "22:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
+    },
+    {
+        name: "23:00",
+        toughened: 0,
+        target: 1000,
+        average: 157.92,
+        broken: 0,
     },
 ];
 
@@ -50,38 +188,55 @@ function StatusChart() {
         <Box sx={{ flexBasis: "50%" }}>
             <h2>Status</h2>
             <TougheningOptions />
-            <ResponsiveChartContainer
-                series={series}
-                height={400}
-                xAxis={[
-                    {
-                        id: "date",
-                        data: DATATIME,
-                        scaleType: "band",
-                        valueFormatter: (value) => value.toString(),
-                    },
-                ]}
-                yAxis={[
-                    {
-                        id: "eco",
-                        scaleType: "linear",
-                    },
-                    {
-                        id: "pib",
-                        scaleType: "log",
-                    },
-                ]}
+            <ResponsiveContainer
+                width="100%"
+                height="100%"
+                minHeight={300}
+                minWidth={400}
             >
-                <BarPlot />
-                <LinePlot />
-                <LinePlot />
-                <ChartsXAxis
-                    label="Date - Time"
-                    position="bottom"
-                    axisId="date"
-                />
-                <ChartsYAxis label="kilograms" position="left" axisId="eco" />
-            </ResponsiveChartContainer>
+                <ComposedChart
+                    width={500}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 20,
+                        right: 20,
+                        bottom: 20,
+                        left: 20,
+                    }}
+                >
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <XAxis dataKey="name" scale="band" />
+                    <YAxis
+                        label={{
+                            value: "Kilograms",
+                            angle: -90,
+                            position: "insideLeft",
+                        }}
+                    />
+                    <Tooltip />
+                    <Legend />
+                    <Line
+                        type="monotone"
+                        dataKey="target"
+                        fill="#8884d8"
+                        stroke="#8884d8"
+                    />
+                    <Bar
+                        dataKey="toughened"
+                        stackId={"a"}
+                        barSize={20}
+                        fill="#413ea0"
+                    />
+                    <Bar
+                        dataKey="broken"
+                        stackId={"a"}
+                        barSize={20}
+                        fill="#ff0de3"
+                    />
+                    <Line type="monotone" dataKey="average" stroke="#ff7300" />
+                </ComposedChart>
+            </ResponsiveContainer>
         </Box>
     );
 }
--- src/components/WaitTimeChart.jsx
+++ src/components/WaitTimeChart.jsx
@@ -1,21 +1,34 @@
-import { BarChart } from "@mui/x-charts/BarChart";
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+} from "recharts";
 
 function WaitTimeChart() {
-    const chartSetting = {
-        yAxis: [
-            {
-                label: "minutes",
-            },
-        ],
-    };
+    const data = [{ name: "2024-03-07", total: 205.38 }];
     return (
-        <BarChart
-            xAxis={[{ scaleType: "band", data: ["2024/03/07"] }]}
-            series={[{ data: [205.38] }]}
-           
-            height={300}
-            {...chartSetting}
-        />
+        <BarChart width={400} height={300} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+                dataKey="name"
+                label={{
+                    value: "Date",
+                    position: "insideBottomRight",
+                    offset: 0,
+                    bottom: 20,
+                }}
+            />
+            <YAxis
+                label={{ value: "Minutes", angle: -90, position: "insideLeft" }}
+            />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="total" fill="#8884d8" />
+        </BarChart>
     );
 }
 
